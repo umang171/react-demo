bd5a7dcc1eaa0c95fe9366b4efb95af9dc7bd487
Umang Gohel <umanggohel17@gmail.com>
2025-01-11 18:40:50 +0530

    counter with useState()

---
+++ counter/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [counter, setCounter] = useState(5);
+  const addCounter = () => {
+    if (counter < 10) {
+      setCounter(counter + 1);
+    }
+  };
+  const removeCounter = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  };
+  return (
+    <>
+      <h1>Umang Counter</h1>
+      <h2>Count:{counter}</h2>
+      <button onClick={addCounter}>Add value</button>
+      <br />
+      <br />
+      <button onClick={removeCounter}>Remove value</button>
+    </>
+  );
+}
+
+export default App;
